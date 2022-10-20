31024768bec7cf1443c26c48ce582f605ff21092
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2022-10-20 20:56:22 +0300

    Add Navigation to Members . and Withdraw templates

--- SavingsManager/teleka/urls.py
+++ SavingsManager/teleka/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/',RegisterPage.as_view(),name='register'),
-    path('creat-member/',CreateMember.as_view(),name='create-member'),
+    path('create-member/',CreateMember.as_view(),name='create-member'),
     path('view-members/',ViewMembers.as_view(),name='view-members'),
     path('member-update/<int:pk>/', MemberUpdate.as_view(), name='member-update'),
     path('member-delete/<int:pk>/', MemberDelete.as_view(), name='member-delete'),
