1c30b26410f2833241198d0ea64e4cc7faf051d9
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2022-10-27 13:37:21 +0300

    Another static commit

--- SavingsManager/SavingsManager/settings.py
+++ SavingsManager/SavingsManager/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'teleka.apps.TelekaConfig',
     'crispy_forms',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'SavingsManager.urls'
@@ -116,5 +118,5 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
